82323ba4c405587814c03758c67813614651f1ad
Pe6o601 <petermarinov93@abv.bg>
2015-03-22 13:45:37 +0200

    character upgrade

--- models/Character.js
+++ models/Character.js
@@ -2,47 +2,54 @@
 var characterModel = (function(){
     var Character;
     Character = (function(){
-        function Character(Strength, Intelligence, Agillity){
-            this.setStrength(Strength);
-            this.setIntelligence(Intelligence);
-            this.setAgillity(Agillity);
+        function Character(strength, intelligence, agillity){
+            this.setStrength(strength);
+            this.setIntelligence(intelligence);
+            this.setAgillity(agillity);
         }
 
         Character.prototype.setStrength = function (value) {
-            this._Strength = value;
-        }
+            this._strength = value;
+        };
 
-        Character.prototype.getStength = function(){
-            return this._Strength;
-        }
+        Character.prototype.getStrength = function(){
+            return this._strength;
+        };
 
         Character.prototype.setIntelligence = function (value) {
-            this._Intelligence = value;
-        }
+            this._intelligence = value;
+        };
 
         Character.prototype.getIntelligence = function(){
-            return this._Intelligence;
-        }
+            return this._intelligence;
+        };
 
         Character.prototype.setAgillity = function (value) {
-            this._Agillity = value;
-        }
+            this._agillity = value;
+        };
 
         Character.prototype.getAgillity = function(){
-            return this._Agillity;
-        }
-
-
+            return this._agillity;
+        };
 
         Character.prototype.InitCharacterStats = function(){
-            this._MaxHealth = this._Strength * 5;
-            this._CurrentHealth = this._MaxHealth;
-            this._MaxMana = this._Intelligence * 5;
-            this._CurrentMana = this._MaxMana;
-            this._PhisicalDamage = this._Strength/2;
-            this._MagicDamage = this._Intelligence/2;
-            this._PhisicalResistance = this._Strength/4;
-            this._MagicResistance = this._Intelligence/4;
+            this._maxHealth = this.getStrength() * 5;
+            this._currentHealth = this.this.getStrength() * 5;
+            this._maxMana = this.getIntelligence() * 5;
+            this._currentMana = this.getIntelligence() * 5;
+            this._phisicalDamage = this.getStrength()/2;
+            this._magicDamage = this.getIntelligence()/2;
+            this._phisicalResistance = this.getStrength()/4;
+            this._magicResistance = this.getIntelligence()/4;
+            this._dodgeChance = this.getAgillity()/10;
+            this.currentDodgeChance =this.getAgillity()/10;
+            this._criticalChance = this.getAgillity()/10;
+            this._currentCriticalChance = this.getAgillity()/20;
         }
+
+        Character.prototype.getCriticalDamage = function(damage){
+            return (damage*(1+this.getAgillity()/100))+0.3;
+        }
+
     }());
 }());
